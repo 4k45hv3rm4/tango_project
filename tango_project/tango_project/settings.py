3640eeef8cd0457527d28f07a6cf8094c7d3c1bf
Aakash Verma <4k45hr0ck5007@gmail.com>
2020-04-26 20:19:04 +0530

    corrected  media configurations

--- tango_project/tango_project/settings.py
+++ tango_project/tango_project/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS=[STATIC_DIR,]
 MEDIA_URL = '/media/'
-MEDIA_DIR = os.path.join(BASE_DIR, "media")
+MEDIA_DIR = os.path.join(BASE_DIR, "media/")
 MEDIA_ROOT = MEDIA_DIR
 
 
--- tango_project/tango_project/urls.py
+++ tango_project/tango_project/urls.py
@@ -22,4 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("rango/", include("rango.urls"))
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
