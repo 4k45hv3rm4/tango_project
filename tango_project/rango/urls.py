0f3fda7803b9f9daa2d2603631ff19bff1235cd1
Aakash Verma <4k45hr0ck5007@gmail.com>
2020-04-28 08:08:50 +0530

    added category and page options and displayed them in template

--- tango_project/rango/urls.py
+++ tango_project/rango/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import index
+from .views import index,show_category
 
 urlpatterns =[
-    url('', index, name="index" ),
+    url(r'^$', index, name="index" ),
+    url(r'^category/(?P<category_url>[\w\-]+)$', show_category, name="category")
 ]
--- tango_project/rango/views.py
+++ tango_project/rango/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.conf import settings
+from .models import Category, Page
+
+
+
 
 def index(request):
-    return render(request, 'rango/index.html')
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context={'categories':category_list}
+    return render(request, 'rango/index.html', context)
+
+
+def show_category(request, category_url):
+    context = {}
+    try:
+        category = Category.objects.get(slug=category_url)
+        pages = Page.objects.filter(category=category)
+        context['pages']=pages
+        context['category']=category
+    except Category.DoesNotExist:
+        context['category'] = None
+        context['pages'] = None
+    return render(request, "rango/category.html", context)
+
