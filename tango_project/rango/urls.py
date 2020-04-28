a1e8eddb02f50846769beb1ffc68402bb2db19c1
Aakash Verma <4k45hr0ck5007@gmail.com>
2020-04-28 23:52:29 +0530

    added signup functionality and template for sign up

--- tango_project/rango/urls.py
+++ tango_project/rango/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url
-from .views import index,show_category
+from .views import index,show_category,add_category, add_page,register,user_login,  user_logout
+
+
 
 urlpatterns =[
     url(r'^$', index, name="index" ),
-    url(r'^category/(?P<category_url>[\w\-]+)$', show_category, name="category")
+    url(r'^add_category/$', add_category, name="add_category"),
+    url(r'^add_page/(?P<category_url>[\w\-]+)$', add_page, name="add_page"),
+    url(r'^category/(?P<category_url>[\w\-]+)$', show_category, name="category"),
+    url(r'^register/$', register, name="register"),
+    url(r'^login/$', user_login, name="login"),
+    url(r'^logout/$', user_logout, name="logout")
+
 ]
