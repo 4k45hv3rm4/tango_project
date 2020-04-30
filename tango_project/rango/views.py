be7e3c188a441520292ddc43afc57fb363b79d60
Aakash Verma <4k45hr0ck5007@gmail.com>
2020-04-30 13:12:12 +0530

    improved invalid login funcitonality

--- tango_project/rango/views.py
+++ tango_project/rango/views.py
@@ -45,6 +45,69 @@ def index(request):
     response = render(request, 'rango/index.html', context)
     return response
 
+# register
+def register(request):
+    registered=False
+
+    if request.method=='POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+            profile.save()
+            registered = True
+            print("registered")
+
+        else:
+            print(user_form.errors,profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    return render(request,
+                'rango/register.html',
+                {
+                'user_form':user_form,
+                'profile_form':profile_form,
+                'registered':registered
+                }
+            )
+
+
+
+def user_login(request):
+    if request.method=="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse('Your rango account is disabled')
+        else:
+            print("Invalid login details {username}, {password}")
+            return render(request, 'rango/Invalid.html', {'message':'Invalid Login Details Supplied'})
+    else:
+        return render(request, 'rango/login.html', {})
+
+@login_required(login_url=LOGIN_REDIRECT_URL)
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+
+
 # Displays category
 @login_required(login_url=LOGIN_REDIRECT_URL)
 def show_category(request, category_url):
@@ -100,63 +163,3 @@ def add_page(request, category_url):
     return render(request, 'rango/add_page.html', context)
 
 
-
-# register
-def register(request):
-    registered=False
-
-    if request.method=='POST':
-        user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
-
-
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-            profile.save()
-            registered = True
-            print("registered")
-
-        else:
-            print(user_form.errors,profile_form.errors)
-    else:
-        user_form = UserForm()
-        profile_form = UserProfileForm()
-    return render(request,
-                'rango/register.html',
-                {
-                'user_form':user_form,
-                'profile_form':profile_form,
-                'registered':registered
-                }
-            )
-
-
-
-def user_login(request):
-    if request.method=="POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
-
-        if user:
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-            else:
-                return HttpResponse('Your rango account is disabled')
-        else:
-            print("Invalid login details {username}, {password}")
-            return HttpResponse('Invalid login details suppled')
-    else:
-        return render(request, 'rango/login.html', {})
-
-@login_required(login_url=LOGIN_REDIRECT_URL)
-def user_logout(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('index'))
